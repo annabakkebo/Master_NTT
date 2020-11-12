aee9f88fcd5364538790200980cf54550f60e06b
annabakkebo <annabakk@stud.ntnu.no>
2020-11-12 13:48:14 +0100

    Attempting with Q equal to 4993 = 2^7*3*13+1

--- params.h
+++ params.h
@@ -5,14 +5,14 @@
 #ifndef NTT_PARAMS_H
 #define NTT_PARAMS_H
 
-#define N 384 // the degree of the modulos polynomial, X^N+1
-#define Q 769 // the modulos
-#define ROOT_OF_UNITY 7 // the PRIMITIVE_N th root of unity
-#define PRIMITIVE_N 256 //to what power the root of unity is 1
-#define LEVEL 7 // how many levels that will be performed
-#define INNVERSE_POWER_OF_TWO 763// the inverse of 2^LEVEL he
+#define N 128 // the degree of the modulos polynomial, X^N+1
+#define Q 4993 // the modulos
+#define ROOT_OF_UNITY 66 // the PRIMITIVE_N th root of unity
+#define PRIMITIVE_N 128 //to what power the root of unity is 1
+#define LEVEL 6 // how many levels that will be performed
+#define INNVERSE_POWER_OF_TWO 4915// the inverse of 2^LEVEL he
 
-#define NUM_POLYNOMIALS 128// number of polynomials in the LEVELth level ie 2^LEVEL
+#define NUM_POLYNOMIALS 64// number of polynomials in the LEVELth level ie 2^LEVEL
 #define COUNTOPERATIONS 0// 0 for not counting and 1 for counting
 
 
