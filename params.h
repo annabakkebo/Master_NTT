db5ac9384143a8bb319f01509d9110e88a513e6e
annabakkebo <annabakk@stud.ntnu.no>
2020-11-12 18:29:41 +0100

    Not working, tried with 12289

--- params.h
+++ params.h
@@ -5,14 +5,14 @@
 #ifndef NTT_PARAMS_H
 #define NTT_PARAMS_H
 
-#define N 768 // the degree of the modulos polynomial, X^N+1
-#define Q 257 // the modulos
-#define ROOT_OF_UNITY 3 // the PRIMITIVE_N th root of unity
-#define PRIMITIVE_N 256 //to what power the root of unity is 1
+#define N 128 // the degree of the modulos polynomial, X^N+1
+#define Q 12289 // the modulos
+#define ROOT_OF_UNITY 41 // the PRIMITIVE_N th root of unity
+#define PRIMITIVE_N 4096 //to what power the root of unity is 1
 #define LEVEL 7 // how many levels that will be performed
-#define INNVERSE_POWER_OF_TWO 255// the inverse of 2^LEVEL he
+#define INNVERSE_POWER_OF_TWO 12193// the inverse of 2^LEVEL he
 
-#define NUM_POLYNOMIALS 128// number of polynomials in the 7th level ie 2^LEVEL
+#define NUM_POLYNOMIALS 128// number of polynomials in the LEVELth level ie 2^LEVEL
 #define COUNTOPERATIONS 0// 0 for not counting and 1 for counting
 
 
