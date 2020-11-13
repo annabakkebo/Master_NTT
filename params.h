9278b36ca2e6182b021f31a82a0376d0b61e633f
annabakkebo <annabakk@stud.ntnu.no>
2020-11-13 11:53:06 +0100

    Q=12289

--- params.h
+++ params.h
@@ -5,14 +5,14 @@
 #ifndef NTT_PARAMS_H
 #define NTT_PARAMS_H
 
-#define N 16384 // the degree of the modulos polynomial, X^N+1
-#define Q 40961 // the modulos
-#define ROOT_OF_UNITY 12 // the PRIMITIVE_N th root of unity
-#define PRIMITIVE_N 8192 //to what power the root of unity is 1
-#define LEVEL 12 // how many levels that will be performed
-#define INNVERSE_POWER_OF_TWO 40951// the inverse of 2^LEVEL he
+#define N 256 // the degree of the modulos polynomial, X^N+1
+#define Q 12289 // the modulos
+#define ROOT_OF_UNITY 41 // the PRIMITIVE_N th root of unity
+#define PRIMITIVE_N 4096 //to what power the root of unity is 1
+#define LEVEL 7 // how many levels that will be performed
+#define INNVERSE_POWER_OF_TWO 12193// the inverse of 2^LEVEL he
 
-#define NUM_POLYNOMIALS 4096// number of polynomials in the LEVELth level ie 2^LEVEL
+#define NUM_POLYNOMIALS 128// number of polynomials in the LEVELth level ie 2^LEVEL
 #define COUNTOPERATIONS 0// 0 for not counting and 1 for counting
 
 
