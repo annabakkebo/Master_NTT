600126f3c1c949094416727c165f5fe264f9a5dc
annabakkebo <annabakk@stud.ntnu.no>
2020-11-13 12:02:20 +0100

    Changed params.h to be 11 levels

--- params.h
+++ params.h
@@ -5,14 +5,14 @@
 #ifndef NTT_PARAMS_H
 #define NTT_PARAMS_H
 
-#define N 256 // the degree of the modulos polynomial, X^N+1
+#define N 12288 // the degree of the modulos polynomial, X^N+1
 #define Q 12289 // the modulos
 #define ROOT_OF_UNITY 41 // the PRIMITIVE_N th root of unity
 #define PRIMITIVE_N 4096 //to what power the root of unity is 1
-#define LEVEL 7 // how many levels that will be performed
-#define INNVERSE_POWER_OF_TWO 12193// the inverse of 2^LEVEL he
+#define LEVEL 11 // how many levels that will be performed
+#define INNVERSE_POWER_OF_TWO 12283// the inverse of 2^LEVEL he
 
-#define NUM_POLYNOMIALS 128// number of polynomials in the LEVELth level ie 2^LEVEL
+#define NUM_POLYNOMIALS 2048// number of polynomials in the LEVELth level ie 2^LEVEL
 #define COUNTOPERATIONS 0// 0 for not counting and 1 for counting
 
 
